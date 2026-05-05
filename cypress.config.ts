c617acbda36e06c12a26232ee7d4f8b85172d8b1
Diego Córdova Rodríguez <A01781166@tec.mx>
2026-05-05 14:32:42 -0600

    feat: added theme-toggle cypress test for use case 7

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,10 @@
+/**
+ * Cypress Configuration File
+ * 
+ * This file contains all the configuration settings for Cypress, 
+ * including environment variables, base URL, and test setup. 
+ */
+
 import 'dotenv/config';
 import { defineConfig } from 'cypress';
 
@@ -7,19 +14,22 @@ export default defineConfig({
     baseUrl: 'https://localhost:4321',
     chromeWebSecurity: false,
     screenshotsFolder: false,
+    testIsolation: false,
     env: {
-      SOLICITANTE_USER: process.env.CYPRESS_SOLICITANTE_USER,
-      SOLICITANTE_PASSWORD: process.env.CYPRESS_SOLICITANTE_PASSWORD,
-      N1_USER: process.env.CYPRESS_N1_USER,
-      N1_PASSWORD: process.env.CYPRESS_N1_PASSWORD,
-      N2_USER: process.env.CYPRESS_N2_USER,
-      N2_PASSWORD: process.env.CYPRESS_N2_PASSWORD,
       ADMIN_USER: process.env.CYPRESS_ADMIN_USER,
       ADMIN_PASSWORD: process.env.CYPRESS_ADMIN_PASSWORD,
+      SUPERADMIN_USER: process.env.CYPRESS_SUPERADMIN_USER,
+      SUPERADMIN_PASSWORD: process.env.CYPRESS_SUPERADMIN_PASSWORD,
+      SOLICITANTE_USER: process.env.CYPRESS_SOLICITANTE_USER,
+      SOLICITANTE_PASSWORD: process.env.CYPRESS_SOLICITANTE_PASSWORD,
+      N2_USER: process.env.CYPRESS_N2_USER,
+      N2_PASSWORD: process.env.CYPRESS_N2_PASSWORD,
+      N1_USER: process.env.CYPRESS_N1_USER,
+      N1_PASSWORD: process.env.CYPRESS_N1_PASSWORD,
       AV_USER: process.env.CYPRESS_AV_USER,
       AV_PASSWORD: process.env.CYPRESS_AV_PASSWORD,
       CPP_USER: process.env.CYPRESS_CPP_USER,
-      CPP_PASSWORD: process.env.CYPRESS_CPP_PASSWORD,
+      CPP_PASSWORD: process.env.CYPRESS_CPP_PASSWORD
     },
   }
 })
